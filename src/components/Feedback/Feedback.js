8f55ad5c2587b12d37c9f14797a395073763be82
LarisaBeskrovna <117515454+LarisaBeskrovna@users.noreply.github.com>
2023-06-27 14:53:03 +0300

    01

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -2,28 +2,24 @@ import PropTypes from 'prop-types';
 
 import css from './feedback.module.css';
 
-const Feedback = ({addGoodRank, addNeutralRank, addBadRank}) => {
-
-    
- return (
+const Feedback = ({ options, onLeaveFeedback }) => {
+  return (
     <div className={css.feedback}>
-    <h1 className={css.feedback_title}>Please leave your feedback</h1>
-    <ul className={css.feedback_btns}>
-    <li className={css.good}>
-    <button onClick={addGoodRank}>Good</button></li>
-    <li className={css.neutral}>
-    <button onClick={addNeutralRank}>Neutral</button></li>
-    <li className={css.bad}>
-    <button onClick={addBadRank}>Bad</button></li>
-    </ul>
+      <h1 className={css.feedback_title}>Please leave your feedback</h1>
+      <ul className={css.feedback_btns}>
+        {options.map(option => (
+          <li className={css.good} key={option}>
+            <button onClick={() => onLeaveFeedback(option)}>{option}</button>
+          </li>
+        ))}
+      </ul>
     </div>
-    )
+  );
 };
 
 Feedback.propTypes = {
-    addGoodRank: PropTypes.func.isRequired,
-    addNeutralRank: PropTypes.func.isRequired,
-    addBadRank: PropTypes.func.isRequired
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
 
 export default Feedback;
